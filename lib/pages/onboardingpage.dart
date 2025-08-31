8ff1259207c007a5f7d38849d3733b6c11bb64b1
Muskan chaudhary <muskanchaudhary152005@gmail.com>
2025-09-01 00:25:06 +0530

    Updated onboarding page, pubspec.yaml and images

--- lib/pages/onboardingpage.dart
+++ lib/pages/onboardingpage.dart
@@ -12,9 +12,56 @@ class _OnboardingpageState extends State<Onboardingpage> {
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        // Image.asset(name)
+        Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/images/onboardng.png"),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+
+        Positioned(
+          top: 540,
+          left: 200,
+          child: Image.asset("assets/images/Group.png", height: 60, width: 60),
+        ),
+
+        Positioned(
+          top: 630,
+          left: 145,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Welcome',
+                style: TextStyle(
+                  fontFamily: 'Gilroy',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 48,
+                  color: Colors.white,
+                  decoration: TextDecoration.none,
+                ),
+              ),
+
+              Positioned(
+                top: 690,
+                right: 90,
+                child: Text(
+                  "to our store",
+                  style: TextStyle(
+                    fontFamily: 'Gilroy',
+                    fontWeight: FontWeight.bold,
+                    fontSize: 48,
+                    color: Colors.white,
+                    decoration: TextDecoration.none,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ],
     );
   }
 }
-
