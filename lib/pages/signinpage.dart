b2559386bfbd1f5d37804bdc04c713d55615f615
Muskan chaudhary <muskanchaudhary152005@gmail.com>
2025-09-04 01:08:58 +0530

    Updated signin page UI with phone number input field

--- lib/pages/signinpage.dart
+++ lib/pages/signinpage.dart
@@ -13,12 +13,97 @@ class _SigninpageState extends State<Signinpage> {
     return Scaffold(
       body: Stack(
         children: [
-          Container(
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
             height: MediaQuery.of(context).size.height / 2,
-            color: Colors.white,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage("assets/images/maskgroup.png"),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                image: DecorationImage(
+                  image: AssetImage("assets/images/maskgroup.png"),
+                ),
+              ),
+            ),
+          ),
+
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            height: MediaQuery.of(context).size.height / 2,
+            child: Container(
+              color: Colors.white,
+              padding: EdgeInsets.only(
+                left: 25.0,
+                right: 10.0,
+                top: 0.0,
+                bottom: 0.0,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    "Get your groceries",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w600,
+                      fontFamily: 'Gilroy',
+                      fontSize: 26,
+                    ),
+                  ),
+                  SizedBox(height: 5),
+                  Text(
+                    "with nectar",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w600,
+                      fontFamily: 'Gilroy',
+                      fontSize: 26,
+                    ),
+                  ),
+
+                  SizedBox(height: 15),
+                  Row(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.symmetric(horizontal: 12,vertical: 16),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                        ),
+                        child: Text("+91"),
+                      ),
+
+                      SizedBox(width: 0),
+                      Expanded(
+                        child: TextField(
+                        keyboardType: TextInputType.phone,
+                        textInputAction: TextInputAction.done,
+                        decoration: InputDecoration(
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.grey,
+                              width: 1.5,
+                            ),
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.grey,
+                              width: 1.5,
+                            ),
+                          )
+                        ),
+                      ),
+                      )
+                    ],
+                  ),
+
+                  SizedBox(height: 15),
+
+
+                ],
               ),
             ),
           ),
