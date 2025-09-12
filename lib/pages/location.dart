b5d1eea4b83addc6d6b1cc263191b87be4f7580a
Muskan chaudhary <muskanchaudhary152005@gmail.com>
2025-09-12 21:47:25 +0530

    Updated verification   page

--- lib/pages/location.dart
+++ lib/pages/location.dart
@@ -35,7 +35,7 @@ class _locationPageState extends State<locationPage> {
           ),
 
           Padding(
-            padding: const EdgeInsets.only(top: 140, left: 115),
+            padding: const EdgeInsets.only(top: 140, left: 110),
             child: SizedBox(
               child: Positioned(
                 top: 150,
@@ -48,22 +48,17 @@ class _locationPageState extends State<locationPage> {
           ),
 
           Positioned(
-            // top: 200,
-            bottom: 0,
-            left: 0,
-            right: 0,
-            // height: MediaQuery.of(context).size.height / 1,
+            top: 340,
+            left: 69,
             child: Container(
               color: Colors.white,
               padding: EdgeInsets.only(
-                left: 25.0,
+                left: 2.0,
                 right: 10.0,
-                top: 1,
                 bottom: 0.0,
               ),
               child: Column(
-                // crossAxisAlignment: CrossAxisAlignment.start,
-                // mainAxisAlignment: MainAxisAlignment.start,
+
                 children: [
                   Text(
                     "Select Your Location",
@@ -75,13 +70,30 @@ class _locationPageState extends State<locationPage> {
                     ),
                   ),
                   SizedBox(height: 5),
-                  Text(
-                    "with nectar",
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.w600,
-                      fontFamily: 'Gilroy',
-                      fontSize: 26,
+                  Padding(
+                    padding: const EdgeInsets.only(left: 5),
+                    child: Text(
+                      "Switch on your location to stay in tune with",
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontWeight: FontWeight.w600,
+                        fontFamily: 'Gilroy',
+                        fontSize: 15,
+                      ),
+                    ),
+                  ),
+
+                  SizedBox(height: 5),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 5),
+                    child: Text(
+                      "what's happening in your area",
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontWeight: FontWeight.w600,
+                        fontFamily: 'Gilroy',
+                        fontSize: 15,
+                      ),
                     ),
                   ),
                 ],
