a0fd5aa6dc294b1819f45d23c569149c30514be8
Muskan chaudhary <muskanchaudhary152005@gmail.com>
2025-09-18 23:05:00 +0530

    updated home page

--- lib/pages/homescreen.dart
+++ lib/pages/homescreen.dart
@@ -14,7 +14,7 @@ class _HomescreenState extends State<Homescreen> {
       appBar: AppBar(
         toolbarHeight: 80,
         title: Padding(
-          padding: const EdgeInsets.only(top: 30,left: 120),
+          padding: const EdgeInsets.only(top: 30,left: 115),
           child: Image.asset(
             "assets/images/carrot.png",
              height: 45,
