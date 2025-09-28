a05beee54756fafacaa429dc91dbd2ea080be9f2
Muskan chaudhary <muskanchaudhary152005@gmail.com>
2025-09-28 21:07:11 +0530

    updated pages && add screens

---
+++ lib/pages/accountPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class accountPage extends StatefulWidget {
+  const accountPage({super.key});
+
+  @override
+  State<accountPage> createState() => _accountPageState();
+}
+
+class _accountPageState extends State<accountPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/pages/favouritePage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class favouritePage extends StatefulWidget {
+  const favouritePage({super.key});
+
+  @override
+  State<favouritePage> createState() => _favouritePageState();
+}
+
+class _favouritePageState extends State<favouritePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
